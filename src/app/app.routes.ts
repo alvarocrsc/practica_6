08923421cda5a561680c6d758b5d43a34d45a818
alvarocrsc <alvarocrsc@gmail.com>
2025-03-20 14:53:59 +0100

    Created 404 component and retouched some styles of Home.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { UserViewComponent } from './pages/user-view/user-view.component';
 import { UsersFormComponent } from './pages/users-form/users-form.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
@@ -9,5 +10,5 @@ export const routes: Routes = [
     { path: 'user/:idUser', component: UserViewComponent },
     { path: 'newuser', component: UsersFormComponent },
     { path: 'updateuser/:idUser', component: UsersFormComponent }, 
-    { path: '**', redirectTo: 'home' }
+    { path: '**', component: Error404Component}
 ];
